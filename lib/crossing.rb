a5e70f831df1c88896ae8d3e2f5f89958dddbfb4
jtulick <jtulick@gmail.com>
2017-02-02 12:06:40 -0600

    added s3 mock test

--- lib/crossing.rb
+++ lib/crossing.rb
@@ -2,9 +2,17 @@ require 'aws-sdk'
 
 # Documentation incoming
 class Crossing
-  def put
+  def initialize(s3_client)
+    if s3_client.nil?
+      raise 'You did not pass in an S3 client...aborting'
+    end
+    @s3_client = s3_client
+  end
+  def put(bucket, key, file)
+    @s3_client.put_object(bucket: bucket, key: key, body: file)
   end
 
-  def get
+  def get(bucket, file)
+
   end
 end
--- spec/crossing_spec.rb
+++ spec/crossing_spec.rb
@@ -1,12 +1,17 @@
+require_relative '../lib/crossing'
+
 RSpec.describe 'Crossing' do
   context 'it can put files' do
     it 'will store the file in s3' do
-
+      s3 = double("AWS::S3::Encryption::Client")
+      allow(s3).to receive(:put_object).with(:bucket=>'bucketname', :key=>'keyname', :body=>'filename')
+      client = Crossing.new(s3)
+      client.put('bucketname', 'keyname', 'filename')
     end
   end
   context 'it can get files' do
     it 'will retrieve the file in s3' do
-      
+
     end
   end
 end
